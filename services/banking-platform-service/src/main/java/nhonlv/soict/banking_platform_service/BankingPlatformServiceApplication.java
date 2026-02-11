1ba421c2a9277926243607e4f67c8809f6efbb95
nhonlv <nhonlv.soict@gmail.com>
2026-02-11 16:07:58 +0000

    inti banking payment service

---
+++ services/banking-platform-service/src/main/java/nhonlv/soict/banking_platform_service/BankingPlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package nhonlv.soict.banking_platform_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPlatformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPlatformServiceApplication.class, args);
+	}
+
+}
---
+++ services/banking-platform-service/src/test/java/nhonlv/soict/banking_platform_service/BankingPlatformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nhonlv.soict.banking_platform_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPlatformServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
